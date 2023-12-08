501aab30bde087686911c6821b827c4328801bec
Fercha Pombo <ferchapombo@gmail.com>
2023-12-08 12:21:03 +0000

    add database, env.py and gitignore file

--- mexicanfoodfeeling/settings.py
+++ mexicanfoodfeeling/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wp7d!pc$)@!*%n$j%e(rycz%hzim)0tsay%b)y$05&fk#^z6&5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+#'django-insecure-wp7d!pc$)@!*%n$j%e(rycz%hzim)0tsay%b)y$05&fk#^z6&5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,11 +80,14 @@ WSGI_APPLICATION = 'mexicanfoodfeeling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
